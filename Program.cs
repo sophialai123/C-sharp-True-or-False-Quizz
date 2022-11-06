bdb31486d5f9566c97a9d65c11100749561dedb9
Sophia <83651475+sophialai123@users.noreply.github.com>
2022-11-06 14:30:05 +1100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace TrueOrFalse
+{
+  class Program
+  {
+		static void Main(string[] args)
+    {
+      // Do not edit these lines
+      Console.WriteLine("Welcome to 'True or False?'\nPress Enter to begin:");
+      string entry = Console.ReadLine();
+      Tools.SetUpInputStream(entry);
+
+      // Type your code below
+      
+    }
+  }
+}
